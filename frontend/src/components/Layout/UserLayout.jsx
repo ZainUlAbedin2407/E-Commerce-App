8d942e7538c4b0a6274ac8bdcc0f083c659414a9
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-06-04 00:21:22 +0500

    first commit

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,17 @@
+import Footer from '../Common/Footer'
+import Header from '../Common/Header'
+
+const UserLayout = () => {
+  return (
+    <>
+    {/* Header */}
+    <Header />    
+    {/* Main Content */}
+    <p className='p-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque ut maiores inventore aspernatur autem possimus exercitationem? Autem, dolor aperiam magnam a non officiis ullam exercitationem quod dolorum excepturi natus sed ex nisi, quis doloremque ipsum. Quasi quisquam porro repellat esse. Maxime eaque, totam modi similique praesentium eius nulla sed ad error at maiores deserunt enim quisquam nisi quos. Reprehenderit dicta in aspernatur tempora placeat? Vel eum aperiam similique cum exercitationem reprehenderit possimus ducimus provident! Totam eum tempora deleniti nulla architecto repudiandae tempore, in velit maiores alias quas ipsa est harum. Aut modi facilis ad, molestiae neque quod consequatur velit nihil.</p>
+    {/* Footer */}
+    <Footer />
+    </>    
+)
+}
+
+export default UserLayout
