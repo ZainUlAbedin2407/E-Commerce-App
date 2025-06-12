cc6064c2a621b45821b16afd72fc29705bb66e2a
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-06-12 21:04:00 +0500

    Completed Collection Page with filterSidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import {Toaster} from "sonner"
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
 function App() {
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/collections/:collection" element={<CollectionPage />} />
         </Route>
         <Route> {/*Admin Layout*/} </Route>
       </Routes>
--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {/* Center Navigation Links */}
         <div className="hidden md:flex space-x-6">
           <Link
-            to="#"
+            to="/collections/all"
             className="text-gray-700 hover:text-black text-sm font-medium uppercase"
           >
             Men
@@ -96,7 +96,7 @@ const Navbar = () => {
           <h2 className="text-xl font-semibold mb-4">Menu</h2>
           <nav className="space-y-4">
             <Link
-              to="#"
+              to="/collections/all"
               onClick={toggleNavDrawer}
               className="block text-gray-600 hover:text-black"
             >
